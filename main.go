4199c1c43af6bb10fbeb9ed5dc0966c9e16e955a
jollaman999 <admin@jollaman999.com>
2026-09-20 19:33:50 +0900

    Raise the version to 3.5.0

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ import (
 	"gopkg.in/natefinch/lumberjack.v2"
 )
 
-const version = "3.4.4"
+const version = "3.5.0"
 
 // Maximum time to wait for in-flight HTTP requests to finish on shutdown.
 const shutdownTimeout = 10 * time.Second
